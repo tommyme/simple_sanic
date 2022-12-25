7d472f33c7c22118fe002cb20326a8743285b6bc
youbowen <56789465+you-bowen@users.noreply.github.com>
2022-12-25 16:46:52 +0800

    restful api

--- add_account.py
+++
@@ -1,9 +0,0 @@
-from main import init, run_async, Account, hashit
-
-import sys
-_, username, password = sys.argv
-
-password = hashit(password)
-
-run_async(init())
-run_async(Account(username=username, password=password).save())
--- utils.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-def hashit(x: str):
-    salt = '114514($#Y%PSQF($ahsdfk23OQWEUR('.encode()
-    s = hashlib.sha3_224()
-    s.update(x.encode()+salt)
-    return s.hexdigest()
