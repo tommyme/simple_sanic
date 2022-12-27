323103c17d6d156f9d0a0353d7213b15974fcf06
youbowen <56789465+you-bowen@users.noreply.github.com>
2022-12-27 12:26:35 +0800

    ..

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from sanic import Sanic
-from sanic.response import text
 import sanic
+from sanic import Sanic
+from sanic.response import text, json
+from sanic_ext import openapi
+from sanic_ext.extensions.openapi.definitions import Parameter, RequestBody, Response
+from pydantic import BaseModel, Field
+
+class Test(BaseModel):
+    foo: str = Field(description="Foo Description", example="FOOO")
+    bar: str = "test"
+
 app = Sanic("hello")
+
+@app.route("/")
+@openapi.definition(
+    summary="ROOT route",
+    description="路由的简介",
+    tag="ybw",
+    parameter=[
+        Parameter("a", str, "query", description="一个简单的query参数", required=True),
+        Parameter("head", str, "header", description="一个简单的header参数", required=True)
+    ],
+    response=Response({"application/json" : Test.schema()}, 200, description="一个简单的response")
+)
+# @openapi.summary("ROOT route")
+# @openapi.description('openapi.description is shown here')
+# @openapi.tag("ybw") # tagged route will grouped together
+# # @openapi.operation('changedName')    # 暂时没什么用 命名冲突的时候可以看看
+# @openapi.parameter("a", str, location="query", required=True)  # location query 表示是get参数，数据类型不能用dict
+# @openapi.parameter("head", str, location="header", required=True)
+# @openapi.response(200, {"application/json" : Test.schema()}, description='hello resp')
+async def test(request: sanic.Request):
+    data = {'headers': dict(request.headers), 'query': dict(request.args)}
+    return json(data)
+
 
 @app.get("/get")    # query
 async def some_get(request: sanic.Request):
